2727da31a322c74a25c08382c0b49a58005932ae
Matheus <Maths.Aki@gmail.com>
2023-01-24 16:13:06 -0300

    app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
-import { Redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
+import Login from './login';
+import Register from './register';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <span className="logo">TRYBE</span>
+  //     <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+  //       Glass
+  //     </object>
+  //     <Route path='/' element={ <Redirect to='/login' /> }/>
+  //   </div>
+  // );
+  // return <Route path='/' element={ <Redirect to='/login' /> }/>;
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-      <Route path='/' element={ <Redirect to="/login" /> }/>
-    </div>
-  );
-  // return <Route path='/' element={ <Redirect to="/login" /> }/>;
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={ <Redirect to='/login' /> }
+        />
+        <Route
+          path='/login'
+          element={ <Login /> }
+        />
+        <Route
+          path='/register'
+          element={ <Register /> }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
