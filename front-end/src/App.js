36ddcf7e74bffe7ea7e298ad977853ff9195f209
Matheus <Maths.Aki@gmail.com>
2023-01-24 14:57:48 -0300

    redirect

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
+import { Redirect } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <span className="logo">TRYBE</span>
+  //     <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+  //       Glass
+  //     </object>
+  //     <Route path='/' element={ <Redirect to="/login" /> }/>
+  //   </div>
+  // );
+  return <Route path='/' element={ <Redirect to="/login" /> }/>;
 }
 
 export default App;
