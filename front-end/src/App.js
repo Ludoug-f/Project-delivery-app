6d627e120be7ae78fca81c69423ed4c8247c0324
Luciano Freitas <lucianoguns123@gmail.com>
2023-01-26 18:24:38 -0300

    ajeitando os elementos da pagina

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-// import Register from './pages/Register';
+import Register from './pages/Register';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
-      {/* <Route path="/register" component={ Register } /> */}
+      <Route path="/register" component={ Register } />
       <Route path="/" component={ Login } />
     </Switch>
   );
