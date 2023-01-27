a879d234d2352ec08452055248daaf93a166b4cb
Luciano Freitas <lucianoguns123@gmail.com>
2023-01-27 00:19:03 -0300

    fix

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const route = require('./Routes');
-const routerProducts = require('./Routes/routerProducts');
+
+const routeUser = require('./Routes/routerUser');
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
-app.use(routerProducts);
+
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', route.routerLogin);
-app.use('/products', route.routerProducts);
+app.use('/regiser', route.routerUser);
+app.use(routeUser);
 
 module.exports = app;
