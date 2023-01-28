194096f456e29785bd21178270959af3c27e3712
Marcos Sobrinho <wintermarck@outlook.com>
2023-01-27 22:00:07 -0300

    Main branch

--- back-end/src/api/Controllers/controllerRegister.js
+++ back-end/src/api/Controllers/controllerRegister.js
@@ -1,5 +1,6 @@
 const { checkUser } = require('../Services/serviceRegister');
 
+// Create new a user in database and return a token
 const NewUser = async (req, res) => {
     const user = await checkUser(req.body);
     if (user) {
