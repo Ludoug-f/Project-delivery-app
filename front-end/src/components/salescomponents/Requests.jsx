00ac80073602304f2f1a3870de785ed35ea4d94c
Matheus <Maths.Aki@gmail.com>
2023-01-30 18:52:27 -0300

    começando com as mudanças

--- front-end/src/components/salescomponents/Requests.jsx
+++ front-end/src/components/salescomponents/Requests.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react';
+import React from 'react'; // , { useState }
 // import { setOrder, setCheck } from '../../pages/Seeler';
 
-export default function Requests({ setOrder, setCheck }) {
+export default function Requests() {
   return (
     <div>
       Orders:
-      {}
+      {/* Aqui era para ter um Map */}
+      <div>
+        <div
+          data-testid="seller_order__element-order-id-<id>"
+          // tem que concerar o ID em cima
+        >
+          Pedido id
+        </div>
+        <div
+          data-testid="seller_order__element-delivery-status-<id>"
+        >
+          StatusID
+        </div>
+      </div>
     </div>
   );
 }
