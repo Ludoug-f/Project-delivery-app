f36d0c53ba47f380942f3bd8602a42a48b07ac8f
Matheus <Maths.Aki@gmail.com>
2023-01-30 20:02:29 -0300

    segundou

--- front-end/src/components/salescomponents/Details.jsx
+++ front-end/src/components/salescomponents/Details.jsx
@@ -4,7 +4,31 @@ export default function Details() {
   return (
     <div>
       Detalhes do Produto:
-      <div></div>
+      <div>
+        <div
+          // Header Lista
+          className="HeaderRequestList"
+        >
+          <div>
+            Pedido
+          </div>
+          <div>
+            Data
+          </div>
+          <div>
+            Status
+          </div>
+          <div>
+            Mudar Info
+          </div>
+        </div>
+        <div
+          // Lista completa
+          className="RequestList"
+        >
+          Lista
+        </div>
+      </div>
     </div>
   )
 }
