aeb1a4df0e69f7d047052300c394aea55a425306
Matheus <Maths.Aki@gmail.com>
2023-01-30 18:01:20 -0300

    vendo aqui

--- front-end/src/pages/Seeler.jsx
+++ front-end/src/pages/Seeler.jsx
@@ -3,19 +3,17 @@ import NavBar from '../components/Navbar';
 // import '../styles/Seeler.css';
 
 export default function Seller() {
-  const [Order, setOrder] = useContext('');
-  const [Check, setCheck] = useContext(false);
+  // const [Order, setOrder] = useContext('');
+  // const [Check, setCheck] = useContext(false);
 
   return (
     <>
       <NavBar />
-      <p>Começando</p>
-      <p>Ah</p>
-      { Check ? <Request
+      {/* { Check ? <Request
         props={ setOrder, setCheck }
       /> : <Details
         props={ setOrder, setCheck }
-      /> }
+      /> } */}
     </>
   );
 }
