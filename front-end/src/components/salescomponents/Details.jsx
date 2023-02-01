02404b3e55916998d66212b929eed6fc5de13681
Matheus <Maths.Aki@gmail.com>
2023-02-01 17:03:16 -0300

    datatestid

--- front-end/src/components/salescomponents/Details.jsx
+++ front-end/src/components/salescomponents/Details.jsx
@@ -10,31 +10,66 @@ export default function Details({ setOrder, setCheck, detailInfo, requestInfo })
     <div>
       Detalhes do Produto:
       <p
-        data-testid="seller_order_details__element-order-details-label-delivery-status"
-      >
-        Pedido
-        { requestInfo.id }
-      </p>
-      { orderStatus === 'pendente'
-        ? <p>
-          Preparar pedido
-        </p>
-        : null}
-      <p
         data-testid={
           `seller_order_details__element-order-details-label-order-${requestInfo.id}`
         }
       >
+        Pedido
+        { requestInfo.id }
+      </p>
+      <p
+        data-testid="seller_order_details__element-order-details-label-delivery-status"
+      >
         { orderStatus }
       </p>
+      { orderStatus === 'pendente' ? (
+        <button
+          type="button"
+          onClick={ () => setOrderStatus('preparando') }
+        >
+          <p>
+            Preparar pedido
+          </p>
+        </button>
+      ) : null }
+      { orderStatus === 'pendente' || orderStatus === 'preparando' ? (
+        <button
+          type="button"
+          onClick={ () => setOrderStatus('saiu') }
+        >
+          <p>
+            Saiu para Entrega
+          </p>
+        </button>
+      ) : null }
+      { orderStatus === 'saiu' || orderStatus === 'preparando' ? (
+        <button
+          type="button"
+          onClick={ () => setOrderStatus('entregue') }
+        >
+          <p>
+            Saiu para Entrega
+          </p>
+        </button>
+      ) : null }
       { detailInfo.map((elem) => (
         <div
           key={ elem.product_id }
-          data-testid={
-            `seller_order_details__element-order-table-name-${elem.product_id}`
-          }
         >
-          { elem.product_id }
+          <p
+            data-testid={
+              `seller_order_details__element-order-table-item-number-${elem.product_id}`
+            }
+          >
+            { elem.product_id }
+          </p>
+          <p
+            data-testid={
+              `seller_order_details__element-order-table-name-${elem.product_id}`
+            }
+          >
+            nome do produto
+          </p>
         </div>
       ))}
     </div>
