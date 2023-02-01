e6a5c81362b2b2ca21b2d9bfbada5bcd3acc27b7
Matheus <Maths.Aki@gmail.com>
2023-02-01 16:42:38 -0300

    state

--- front-end/src/components/salescomponents/Details.jsx
+++ front-end/src/components/salescomponents/Details.jsx
@@ -1,16 +1,29 @@
 /* eslint-disable react/prop-types */
 import React from 'react'; // , { useState }
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 // import { detailsMock } from '../salescomponents/Mocks';
 
 export default function Details({ setOrder, setCheck, detailInfo, requestInfo }) {
   console.log('sets: ', setOrder, setCheck, ' == details: ', detailInfo, requestInfo);
+  const [orderStatus, setOrderStatus] = useState('');
   return (
     <div>
       Detalhes do Produto:
+      <p
+        data-testid={
+          `seller_order_details__element-order-details-label-order-${requestInfo.id}`
+        }
+      >
+        Pedido
+        { requestInfo.id }
+      </p>
       { detailInfo.map((elem) => (
         <div
           key={ elem.product_id }
+          data-testid={
+            `seller_order_details__element-order-table-name-${elem.product_id}`
+          }
         >
           { elem.product_id }
         </div>
