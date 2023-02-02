f433352580fda88a68a8d71a740764fe80237957
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2023-02-02 13:32:43 -0300

    fluxo admin

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const route = require('./Routes');
-<<<<<<< HEAD
 
 const routeUser = require('./Routes/routerUser');
 const admin = require('./Routes/routerAdmin');
-=======
 const routerProducts = require('./Routes/routerProducts');
 const routerIMG = require('./Routes/imagesRoutes/routeIMG');
 const routerRegister = require('./Routes/routerRegister');
->>>>>>> main-group-14-main
 
 const app = express();
 app.use(express.json());
@@ -19,13 +16,10 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(routerProducts);
 app.use('/images', routerIMG);
 app.use('/login', route.routerLogin);
-<<<<<<< HEAD
 // app.use('/regiser', route.routerUser);
 app.use(routeUser);
 app.use(admin);
-=======
 app.use('/register', route.routerRegister);
 app.use(routerRegister);
->>>>>>> main-group-14-main
 
 module.exports = app;
