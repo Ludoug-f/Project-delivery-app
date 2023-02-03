9d9bc6757b59118963b8bc74a138d9f53306e875
Marcos Sobrinho <wintermarck@outlook.com>
2023-02-03 09:40:04 -0300

    small fix

--- back-end/src/api/Controllers/controllerUser.js
+++ back-end/src/api/Controllers/controllerUser.js
@@ -1,27 +1,24 @@
-const { getAllSellers,
-    getSellerbyId,
-    getUserbyId,
-    getAllUsers } = require('../Services/serviceUser');
+const serviceUser = require('../Services/serviceUser');
 
     const getAllSellersC = async (req, res) => {
-        const sellers = await getAllSellers();
+        const sellers = await serviceUser.getAllSellers();
         return res.status(200).json(sellers);
     };
 
     const getSellerbyIdC = async (req, res) => {
         const { email } = req.params;
-        const seller = await getSellerbyId(email);
+        const seller = await serviceUser.getSellerbyId(email);
         return res.status(200).json(seller);
     };
 
     const getUserbyIdC = async (req, res) => {
         const { email } = req.params;
-        const user = await getUserbyId(email);
+        const user = await serviceUser.getUserbyId(email);
         return res.status(200).json(user);
     };
 
     const getAllUsersC = async (req, res) => {
-        const users = await getAllUsers();
+        const users = await serviceUser.getAllUsers();
         return res.status(200).json(users);
     };
 
